06f4285f5ffe9fb5f4bd05c767ea21d028e81dd9
Felix-Redwood <Felix@grahamredwood.com>
2020-06-03 13:49:02 +0000

    Added AWS S3 config

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -144,8 +144,17 @@ USE_TZ = True
 
 AWS_S3_OBJECT_PARAMETERS = {
     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-    
+    'CacheControl': 'max-age=94608000',
 }
+
+AWS_STORAGE_BUCKET_NAME = 'ecommerce-felix-redwood'
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
