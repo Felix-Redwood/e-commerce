a562e3108c0793647fa17d6c85669639f2f1209e
Felix-Redwood <Felix@grahamredwood.com>
2020-06-03 13:48:46 +0000

    Fixed faulty add_to_cart view

--- cart/views.py
+++ cart/views.py
@@ -13,7 +13,10 @@ def add_to_cart(request, id):
     quantity = int(request.POST.get("quantity"))
     print(quantity)
     cart = request.session.get('cart', {})
-    cart[id] = cart.get(id, quantity)
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity
+    else:
+        cart[id] = cart.get(id, quantity) 
     print(cart)
     request.session['cart'] = cart
     return redirect(reverse('index'))
